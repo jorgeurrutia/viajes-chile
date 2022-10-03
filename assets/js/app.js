213e7fa85700741509ecb7b2db1e06775ec5e634
Jorge Urrutia <jorge.urrutia@live.cl>
2022-10-03 19:52:55 -0300

    Se agrega formulario de contacto y footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,21 @@ $(function(){
     $(".card-title").click(function(){
         $(".card-text").toggle();
     });
+
+    $('#send').click(function(){
+        alert('El mensaje ya fue enviado');
+    });
 });
+
+// Ocultar navbar al hacer scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } 
+    else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
